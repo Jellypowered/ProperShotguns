4ca983f579f63064bfa6455beccafc86324455f6
Jelly <Jellypowered@gmail.com>
2026-02-24 01:15:12 -0600

    Fixed crash - removed problematic ExtraDamages patch, kept armor penetration division

--- Source/HarmonyPatches/Patch_Projectile.cs
+++ Source/HarmonyPatches/Patch_Projectile.cs
@@ -97,45 +97,5 @@ namespace ProperShotguns
                 }
             }
         }
-
-        // Transpiler would be better, but this postfix modifies the list that gets returned
-        [HarmonyPatch(typeof(ProjectileProperties))]
-        [HarmonyPatch("ExtraDamages", MethodType.Getter)]
-        public static class Get_ExtraDamages
-        {
-            public static void Postfix(ProjectileProperties __instance, ref List<ExtraDamage> __result)
-            {
-                if (!ProperShotgunsSettings.divideSecondaryEffects)
-                    return;
-
-                // Get the projectile def that owns these properties
-                ThingDef projectileDef = DefDatabase<ThingDef>.AllDefs.FirstOrDefault(
-                    def => def.projectile == __instance
-                );
-
-                if (projectileDef != null && ShotgunExtension.Get(projectileDef).pelletCount != 0)
-                {
-                    var shotgunExtension = ShotgunExtension.Get(projectileDef);
-
-                    // Create a new list with divided damage amounts
-                    if (__result != null && __result.Count > 0)
-                    {
-                        List<ExtraDamage> modifiedDamages = new List<ExtraDamage>();
-                        foreach (var extraDamage in __result)
-                        {
-                            ExtraDamage divided = new ExtraDamage
-                            {
-                                def = extraDamage.def,
-                                amount = Mathf.RoundToInt(extraDamage.amount / (float)shotgunExtension.pelletCount),
-                                chance = extraDamage.chance,
-                                armorPenetration = extraDamage.armorPenetration / shotgunExtension.pelletCount
-                            };
-                            modifiedDamages.Add(divided);
-                        }
-                        __result = modifiedDamages;
-                    }
-                }
-            }
-        }
     }
 }
